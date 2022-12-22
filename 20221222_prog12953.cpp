4fbb2b6012ffbd5727bde0bbe23b0cb5a7ef5dbb
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2022-12-22 23:30:34 +0900

    Create 20221222_prog12953.cpp

---
+++ 20221222_prog12953.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+using namespace std;
+//유클리디언 알고리즘
+int Euclidian(int x,int y){
+    if(x<y) swap(x,y);
+    while(y>0){
+        int r = x%y;
+        x = y;
+        y = r;
+    }
+    return x;
+}
+
+int solution(vector<int> arr) {
+    int answer = arr[0];
+    int g = arr[0];
+    for(int i=0;i<arr.size();i++){
+        g = Euclidian(answer,arr[i]);
+        answer = (answer*arr[i])/g;
+    }
+    return answer;
+}
