4397e896be254f02bf734b3d63c7b6561f333afe
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2022-12-31 23:54:28 +0900

    Create 20221231_18108.cpp

---
+++ 20221231_18108.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //빠른 입력
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n;
+    cin>>n;
+    cout<<n-543;
+    return 0;
+}
